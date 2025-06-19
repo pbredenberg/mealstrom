92207930c8d702ab69aeeaf233b8e364eb7c0f3d
PaulBredenberg <paulbredenberg@gmail.com>
2025-06-19 17:22:30 -0400

    feat: navbar and dock components
    Also adds theme

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,49 @@ async function greet() {
 </script>
 
 <template>
+  <div className="navbar bg-base-100 shadow-sm sticky top-0">
+    <div className="flex-none">
+      <button className="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="inline-block h-5 w-5 stroke-current"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          ></path>
+        </svg>
+      </button>
+    </div>
+    <div className="flex-1">
+      <span className="btn btn-ghost text-2xl uppercase font-brand">
+        Mealstrom
+      </span>
+    </div>
+    <div className="flex-none">
+      <button className="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="inline-block h-5 w-5 stroke-current"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  </div>
   <main
-    class="flex flex-col items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 font-sans"
+    class="flex flex-col items-center justify-center min-h-[calc(100vh-150px)] bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 font-sans"
   >
     <h1 class="text-3xl font-bold mb-8">Welcome to Tauri + Vue</h1>
 
@@ -63,4 +104,105 @@ async function greet() {
     </form>
     <p class="text-xl font-medium">{{ greetMsg }}</p>
   </main>
+  <div className="dock sticky bottom-0">
+    <button>
+      <svg
+        className="size-[1.2em]"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt">
+          <polyline
+            points="1 11 12 2 23 11"
+            fill="none"
+            stroke="currentColor"
+            stroke-miterlimit="10"
+            strokeWidth="2"
+          ></polyline>
+          <path
+            d="m5,13v7c0,1.105.895,2,2,2h10c1.105,0,2-.895,2-2v-7"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="square"
+            stroke-miterlimit="10"
+            strokeWidth="2"
+          ></path>
+          <line
+            x1="12"
+            y1="22"
+            x2="12"
+            y2="18"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="square"
+            stroke-miterlimit="10"
+            strokeWidth="2"
+          ></line>
+        </g>
+      </svg>
+      <span className="dock-label">Home</span>
+    </button>
+
+    <button className="dock-active">
+      <svg
+        className="size-[1.2em]"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt">
+          <polyline
+            points="3 14 9 14 9 17 15 17 15 14 21 14"
+            fill="none"
+            stroke="currentColor"
+            stroke-miterlimit="10"
+            strokeWidth="2"
+          ></polyline>
+          <rect
+            x="3"
+            y="3"
+            width="18"
+            height="18"
+            rx="2"
+            ry="2"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="square"
+            stroke-miterlimit="10"
+            strokeWidth="2"
+          ></rect>
+        </g>
+      </svg>
+      <span className="dock-label">Inbox</span>
+    </button>
+
+    <button>
+      <svg
+        className="size-[1.2em]"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt">
+          <circle
+            cx="12"
+            cy="12"
+            r="3"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="square"
+            stroke-miterlimit="10"
+            strokeWidth="2"
+          ></circle>
+          <path
+            d="m22,13.25v-2.5l-2.318-.966c-.167-.581-.395-1.135-.682-1.654l.954-2.318-1.768-1.768-2.318.954c-.518-.287-1.073-.515-1.654-.682l-.966-2.318h-2.5l-.966,2.318c-.581.167-1.135.395-1.654.682l-2.318-.954-1.768,1.768.954,2.318c-.287.518-.515,1.073-.682,1.654l-2.318.966v2.5l2.318.966c.167.581.395,1.135.682,1.654l-.954,2.318,1.768,1.768,2.318-.954c.518.287,1.073.515,1.654.682l.966,2.318h2.5l.966-2.318c.581-.167,1.135-.395,1.654-.682l2.318.954,1.768-1.768-.954-2.318c.287-.518.515-1.073.682-1.654l2.318-.966Z"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="square"
+            stroke-miterlimit="10"
+            strokeWidth="2"
+          ></path>
+        </g>
+      </svg>
+      <span className="dock-label">Settings</span>
+    </button>
+  </div>
 </template>
